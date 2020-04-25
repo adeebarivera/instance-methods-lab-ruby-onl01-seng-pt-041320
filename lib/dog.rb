be357780cc604fb2b6798f6fbc81cffec2fa5521
adeeba reaz <adeebarivera@outlook.com>
2020-04-25 01:38:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+# Describe an instance method.
+# Call instance methods on an object.
+# Build instance methods for an object.
+
+class Dog
+def bark
+  puts "Woof!"
+end
+
+def sit
+  puts "The Dog is sitting"
+end
+end
+
