d1584cb53f2dfb52645e17f9f21cbae0ead42aa8
adeeba reaz <adeebarivera@outlook.com>
2020-04-25 01:36:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,3 +2,13 @@
 # Call instance methods on an object.
 # Build instance methods for an object.
 
+class Dog
+def bark
+  puts "Woof!"
+end
+
+def sit
+  puts "The Dog is sitting"
+end
+end
+© 2020 GitHub, In
